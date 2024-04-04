344708c2dcee9bb32cc5b53bc306f6c77c32ec18
LeeJonghoon <jonghoon.1738@gmail.com>
2024-04-05 00:28:16 +0900

    add bullet

---
+++ space_rock/Bullet/Bullet.cs
@@ -0,0 +1,45 @@
+using Godot;
+using System;
+
+public partial class Bullet : Area2D
+{
+	[Export] public int Speed { get; set; } = 1000;
+
+	public Vector2 Velocity = Vector2.Zero;
+
+	public void Start(Transform2D transform)
+	{
+		this.Transform = transform;
+		Velocity = Transform.X * Speed;
+	}
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		this.Position += Velocity * (float)delta;
+	}
+	
+	private void _on_visible_on_screen_notifier_2d_screen_exited()
+	{
+		QueueFree();
+	}
+	
+	private void _on_body_entered(Node2D body)
+	{
+		if (body.IsInGroup("rocks"))
+		{
+			//todo : 추후 rock그룹에 추가하고 explode추가 
+			//body.Explode();
+			QueueFree();
+		}
+	}
+}
+
+
+
+
+
+
